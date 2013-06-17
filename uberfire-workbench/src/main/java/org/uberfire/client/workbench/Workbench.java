69ee15126e1a245ff76a5f9083e08e45f1086aee
Alexandre Porcelli <alexandre.porcelli@gmail.com>
2013-06-17 13:02:23 -0300

    improvements on css styles. now tabs shouldn't be hidden and doesn't need to add any css file to your app.html

--- uberfire-workbench/src/main/java/org/uberfire/client/workbench/Workbench.java
+++ uberfire-workbench/src/main/java/org/uberfire/client/workbench/Workbench.java
@@ -45,7 +45,6 @@ import org.uberfire.client.workbench.widgets.dnd.WorkbenchDragAndDropManager;
 import org.uberfire.client.workbench.widgets.dnd.WorkbenchPickupDragController;
 import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBarPresenter;
 import org.uberfire.client.workbench.widgets.panels.PanelManager;
-import org.uberfire.client.workbench.widgets.toolbar.WorkbenchToolBarPresenter;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
 import org.uberfire.workbench.model.PanelDefinition;
 import org.uberfire.workbench.model.PerspectiveDefinition;
@@ -81,12 +80,6 @@ public class Workbench
     @Inject
     private WorkbenchMenuBarPresenter menuBarPresenter;
 
-//    @Inject
-//    private WorkbenchToolBarPresenter toolBarPresenter;
-
-//    @Inject
-//    private WorkbenchStatusBarPresenter statusBarPresenter;
-
     @Inject
     private Caller<WorkbenchServices> wbServices;
 
@@ -95,18 +88,11 @@ public class Workbench
         if ( !Window.Location.getParameterMap().containsKey( "standalone" ) ) {
             container.add( menuBarPresenter.getView() );
         }
-        //Menu bar
-
-//        //Tool bar
-//        container.add( toolBarPresenter.getView() );
 
         //Container panels for workbench
         workbenchContainer = dragController.getBoundaryPanel();
         workbenchContainer.add( workbench );
         container.add( workbenchContainer );
-
-//        //Status bar
-//        container.add( statusBarPresenter.getView() );
 
         initWidget( container );
     }
@@ -209,8 +195,6 @@ public class Workbench
     private void doResizeWorkbenchContainer( final int width,
                                              final int height ) {
         final int menuBarHeight = menuBarPresenter.getView().asWidget().getOffsetHeight();
-//        final int toolBarHeight = toolBarPresenter.getHeight();
-//        final int statusBarHeight = statusBarPresenter.getView().asWidget().getOffsetHeight();
         final int availableHeight;
         if ( !Window.Location.getParameterMap().containsKey( "standalone" ) ) {
             availableHeight = height - menuBarHeight;
@@ -218,7 +202,6 @@ public class Workbench
             availableHeight = height;
         }
 
-//        final int availableHeight = height - menuBarHeight - toolBarHeight;// - statusBarHeight;
         workbenchContainer.setPixelSize( width, availableHeight );
         workbench.setPixelSize( width, availableHeight );
 
@@ -229,5 +212,4 @@ public class Workbench
             }
         }
     }
-
 }
