mbarkley/uberfire-all

BZ-1079548 - KIE-WB does not honor added repository roles until restart

uberfire/uberfire-api/src/main/java/org/uberfire/workbench/type/Cacheable.java
@@ -1,0 +1,16 @@
+package org.uberfire.workbench.type;
+
+public interface Cacheable {
+
+    /**
+     * Instructs any component that would like to cache the object
+     * that in case it's already cached it should be refreshed inside the cache
+     * @return
+     */
+    boolean requiresRefresh();
+
+    /**
+     * Marks the instance as cached. It should be done by the last cache in chain
+     */
+    void markAsCached();
+}

uberfire/uberfire-security/uberfire-security-api/src/main/java/org/uberfire/security/impl/authz/RuntimeResourceDecisionManager.java
@@ -32,6 +32,7 @@ import org.uberfire.security.authz.RoleDecisionManager;
 import org.uberfire.security.authz.RolesResource;
 import org.uberfire.security.authz.RuntimeResource;
 import org.uberfire.security.authz.VotingStrategy;
+import org.uberfire.workbench.type.Cacheable;
 
 import static org.uberfire.commons.validation.PortablePreconditions.*;
 import static org.uberfire.security.authz.AuthorizationResult.*;
@@ -66,10 +67,13 @@ public class RuntimeResourceDecisionManager implements ResourceDecisionManager {
         if (!(resource instanceof RuntimeResource)) {
             throw new IllegalArgumentException("Parameter named 'resource' is not instance of clazz 'RuntimeResource'!");
         }
-
+        boolean refreshCache = false;
+        if (resource instanceof Cacheable) {
+            refreshCache = ((Cacheable) resource).requiresRefresh();
+        }
         final RuntimeResource runtimeResource = (RuntimeResource) resource;
 
-        if (cache.notContains(subject, runtimeResource)) {
+        if (cache.notContains(subject, runtimeResource) || refreshCache) {
             if (!resourceManager.requiresAuthentication(runtimeResource)) {
                 return ACCESS_ABSTAIN;
             }
@@ -113,6 +117,9 @@ public class RuntimeResourceDecisionManager implements ResourceDecisionManager {
             } else {
                 cache.put(subject, runtimeResource, result);
             }
+            if (resource instanceof Cacheable) {
+                ((Cacheable) resource).markAsCached();
+            }
         }
 
         return cache.get(subject, runtimeResource);
@@ -137,7 +144,8 @@ public class RuntimeResourceDecisionManager implements ResourceDecisionManager {
                 internal.put(resource.getSignatureId(), new HashMap<String, AuthorizationResult>());
             }
             final Map<String, AuthorizationResult> result = internal.get(resource.getSignatureId());
-            if (result.containsKey(subject.getName())) {
+            AuthorizationResult knowValue = result.get(subject.getName());
+            if (result.containsKey(subject.getName()) && knowValue.equals(authzResult)) {
                 return;
             }
             result.put(subject.getName(), authzResult);

uberfire/uberfire-security/uberfire-security-api/src/main/java/org/uberfire/security/impl/authz/RuntimeResourceManager.java
@@ -27,6 +27,7 @@ import org.uberfire.security.ResourceManager;
 import org.uberfire.security.Role;
 import org.uberfire.security.authz.RuntimeResource;
 import org.uberfire.security.impl.RoleImpl;
+import org.uberfire.workbench.type.Cacheable;
 
 import static java.util.Collections.*;
 
@@ -35,10 +36,6 @@ public class RuntimeResourceManager implements ResourceManager {
     final Map<String, RuntimeRestriction> restrictions = new HashMap<String, RuntimeRestriction>();
 
     private RuntimeRestriction addResource( final RuntimeResource resource ) {
-
-        if ( restrictions.containsKey( resource.getSignatureId() ) ) {
-            return null;
-        }
 
         final RuntimeRestriction runtimeRestriction = new RuntimeRestriction( resource.getRoles(), resource.getTraits() );
         restrictions.put( resource.getSignatureId(), runtimeRestriction );
@@ -64,11 +61,16 @@ public class RuntimeResourceManager implements ResourceManager {
             throw new IllegalArgumentException( "Parameter named 'resource' is not instance of clazz 'RuntimeResource'!" );
         }
 
+        boolean refreshCache = false;
+        if (resource instanceof Cacheable) {
+            refreshCache = ((Cacheable) resource).requiresRefresh();
+        }
+
         final RuntimeResource runtimeResource = (RuntimeResource) resource;
 
         RuntimeRestriction restriction = restrictions.get( runtimeResource.getSignatureId() );
 
-        if ( restriction == null ) {
+        if ( restriction == null || refreshCache ) {
             restriction = addResource( runtimeResource );
         }
 

AppFormer/uberfire

Conflicts:
	uberfire-backend/uberfire-backend-api/src/main/java/org/uberfire/backend/organizationalunit/impl/OrganizationalUnitImpl.java
	uberfire-backend/uberfire-backend-api/src/main/java/org/uberfire/backend/repositories/impl/git/GitRepository.java
in order to be able to backport it to 0.3.x had to move Cacheable interface to commons module in order to avoid circular reference between security-api and -api

uberfire-commons/src/main/java/org/uberfire/commons/data/Cacheable.java
@@ -1,0 +1,16 @@
+package org.uberfire.commons.data;
+
+public interface Cacheable {
+
+    /**
+     * Instructs any component that would like to cache the object
+     * that in case it's already cached it should be refreshed inside the cache
+     * @return
+     */
+    boolean requiresRefresh();
+
+    /**
+     * Marks the instance as cached. It should be done by the last cache in chain
+     */
+    void markAsCached();
+}

uberfire-security/uberfire-security-api/src/main/java/org/uberfire/security/impl/authz/RuntimeResourceDecisionManager.java
@@ -20,6 +20,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.uberfire.commons.data.Cacheable;
 import org.uberfire.security.Resource;
 import org.uberfire.security.Role;
 import org.uberfire.security.Subject;
@@ -66,10 +67,13 @@ public class RuntimeResourceDecisionManager implements ResourceDecisionManager {
         if (!(resource instanceof RuntimeResource)) {
             throw new IllegalArgumentException("Parameter named 'resource' is not instance of clazz 'RuntimeResource'!");
         }
-
+        boolean refreshCache = false;
+        if (resource instanceof Cacheable) {
+            refreshCache = ((Cacheable) resource).requiresRefresh();
+        }
         final RuntimeResource runtimeResource = (RuntimeResource) resource;
 
-        if (cache.notContains(subject, runtimeResource)) {
+        if (cache.notContains(subject, runtimeResource) || refreshCache) {
             if (!resourceManager.requiresAuthentication(runtimeResource)) {
                 return ACCESS_ABSTAIN;
             }
@@ -113,6 +117,9 @@ public class RuntimeResourceDecisionManager implements ResourceDecisionManager {
             } else {
                 cache.put(subject, runtimeResource, result);
             }
+            if (resource instanceof Cacheable) {
+                ((Cacheable) resource).markAsCached();
+            }
         }
 
         return cache.get(subject, runtimeResource);
@@ -137,7 +144,8 @@ public class RuntimeResourceDecisionManager implements ResourceDecisionManager {
                 internal.put(resource.getSignatureId(), new HashMap<String, AuthorizationResult>());
             }
             final Map<String, AuthorizationResult> result = internal.get(resource.getSignatureId());
-            if (result.containsKey(subject.getName())) {
+            AuthorizationResult knowValue = result.get(subject.getName());
+            if (result.containsKey(subject.getName()) && knowValue.equals(authzResult)) {
                 return;
             }
             result.put(subject.getName(), authzResult);

uberfire-security/uberfire-security-api/src/main/java/org/uberfire/security/impl/authz/RuntimeResourceManager.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.uberfire.commons.data.Cacheable;
 import org.uberfire.security.Resource;
 import org.uberfire.security.ResourceManager;
 import org.uberfire.security.Role;
@@ -35,10 +36,6 @@ public class RuntimeResourceManager implements ResourceManager {
     final Map<String, RuntimeRestriction> restrictions = new HashMap<String, RuntimeRestriction>();
 
     private RuntimeRestriction addResource( final RuntimeResource resource ) {
-
-        if ( restrictions.containsKey( resource.getSignatureId() ) ) {
-            return null;
-        }
 
         final RuntimeRestriction runtimeRestriction = new RuntimeRestriction( resource.getRoles(), resource.getTraits() );
         restrictions.put( resource.getSignatureId(), runtimeRestriction );
@@ -64,11 +61,16 @@ public class RuntimeResourceManager implements ResourceManager {
             throw new IllegalArgumentException( "Parameter named 'resource' is not instance of clazz 'RuntimeResource'!" );
         }
 
+        boolean refreshCache = false;
+        if (resource instanceof Cacheable) {
+            refreshCache = ((Cacheable) resource).requiresRefresh();
+        }
+
         final RuntimeResource runtimeResource = (RuntimeResource) resource;
 
         RuntimeRestriction restriction = restrictions.get( runtimeResource.getSignatureId() );
 
-        if ( restriction == null ) {
+        if ( restriction == null || refreshCache ) {
             restriction = addResource( runtimeResource );
         }
 