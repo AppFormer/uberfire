f0613979ff1d3558195c96444dd86c45052d0cc2
Mark Coble <markcoble@markcoble.com>
2017-05-22 11:19:39 +0100

    reverted commit

--- uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/security/adapter/GroupAdapterAuthorizationSource.java
+++ uberfire-backend/uberfire-backend-server/src/main/java/org/uberfire/backend/server/security/adapter/GroupAdapterAuthorizationSource.java
@@ -20,19 +20,20 @@ import org.jboss.errai.security.shared.api.Group;
 import org.jboss.errai.security.shared.api.GroupImpl;
 import org.jboss.errai.security.shared.api.Role;
 import org.jboss.errai.security.shared.api.RoleImpl;
+
 import org.uberfire.backend.server.security.RoleRegistry;
 import org.uberfire.security.authz.adapter.GroupsAdapter;
 
 import javax.security.auth.Subject;
+
+
 import java.util.*;
 
 public class GroupAdapterAuthorizationSource {
 
-    private final ServiceLoader<GroupsAdapter> groupsAdapterServiceLoader = ServiceLoader.load( GroupsAdapter.class );
+    private final ServiceLoader<GroupsAdapter> groupsAdapterServiceLoader = ServiceLoader.load(GroupsAdapter.class);
 
-    protected List<String> loadEntitiesFromSubjectAndAdapters( String username,
-                                                          Subject subject,
-                                                          String[] rolePrincipleNames ) {
+    protected List<String> loadEntitiesFromSubjectAndAdapters(String username, Subject subject, String[] rolePrincipleNames) {
         List<String> roles = new ArrayList<String>();
         try {
 
@@ -40,9 +41,10 @@ public class GroupAdapterAuthorizationSource {
             roles.addAll( filterValidPrincipals( principals ) );
 
             List<String> principalsFromAdapters = collectEntitiesFromAdapters( username, subject );
-            roles.addAll( filterValidPrincipals(principalsFromAdapters ) );
+            roles.addAll( filterValidPrincipals(principalsFromAdapters ) );            
         } catch ( Exception e ) {
             throw new RuntimeException( e );
+
         }
         return roles;
     }
@@ -61,52 +63,51 @@ public class GroupAdapterAuthorizationSource {
 
     protected List<String> collectEntitiesFromAdapters( String username,
                                                 Subject subject ) {
+
         Set<String> userGroups = new HashSet<String>();
-        for ( final GroupsAdapter adapter : groupsAdapterServiceLoader ) {
-            final List<Group> groupRoles = adapter.getGroups( username, subject );
-            if ( groupRoles != null ) {
+        for (final GroupsAdapter adapter : groupsAdapterServiceLoader) {
+            final List<Group> groupRoles = adapter.getGroups(username, subject);
+            if (groupRoles != null) {
                 for (Group group : groupRoles) {
-                    userGroups.add( group.getName() );
+                    userGroups.add(group.getName());
                 }
             }
         }
 
-        return new LinkedList<String>( userGroups );
+        return new LinkedList<String>(userGroups);
     }
 
     /**
      * Collects the principals for a given subject.
      */
-    protected List<String> collectEntitiesFromSubject( String username,
-                                               Subject subject,
-                                               String[] rolePrincipleNames ) {
-        if ( null == subject ) {
+    protected List<String> collectEntitiesFromSubject(String username, Subject subject, String[] rolePrincipleNames) {
+        if (null == subject) {
             return null;
         }
 
         List<String> roles = new ArrayList<String>();
         try {
             Set<java.security.Principal> principals = subject.getPrincipals();
-            if ( principals != null ) {
-                for ( java.security.Principal p : principals ) {
-                    if ( p instanceof java.security.acl.Group ) {
-                        for ( final String rolePrincipleName : rolePrincipleNames ) {
-                            if ( rolePrincipleName.equalsIgnoreCase( p.getName() ) ) {
-                                Enumeration<? extends java.security.Principal> groups = ( ( java.security.acl.Group ) p ).members();
-                                while ( groups.hasMoreElements() ) {
+            if (principals != null) {
+                for (java.security.Principal p : principals) {
+                    if (p instanceof java.security.acl.Group) {
+                        for (final String rolePrincipleName : rolePrincipleNames) {
+                            if (rolePrincipleName.equalsIgnoreCase(p.getName())) {
+                                Enumeration<? extends java.security.Principal> groups = ((java.security.acl.Group) p).members();
+                                while (groups.hasMoreElements()) {
                                     final java.security.Principal groupPrincipal = groups.nextElement();
-                                    roles.add( groupPrincipal.getName() );
+                                    roles.add(groupPrincipal.getName());
                                 }
                             }
                         }
 
                     } else {
-                        roles.add(p.getName() );
+                        roles.add(p.getName());
                     }
                 }
             }
-        } catch ( Exception e ) {
-            throw new RuntimeException( e );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
         return roles;
     }
@@ -115,21 +116,21 @@ public class GroupAdapterAuthorizationSource {
      * For a given collection of principal names, return the Role instances for the ones
      * that are considered roles, so the ones that exist on the RoleRegistry.
      */
-    protected List<Role> getRoles( List<String> principals ) {
+    protected List<Role> getRoles(List<String> principals) {
 
-        if ( null != principals && !principals.isEmpty() ) {
+        if (null != principals && !principals.isEmpty()) {
 
             Set<Role> registeredRoles = RoleRegistry.get().getRegisteredRoles();
 
-            if ( null != registeredRoles && !registeredRoles.isEmpty() ) {
+            if (null != registeredRoles && !registeredRoles.isEmpty()) {
 
                 List<Role> result = new LinkedList<Role>();
 
-                for ( String role : principals ) {
+                for (String role : principals) {
 
-                    if ( null != RoleRegistry.get().getRegisteredRole( role ) ) {
+                    if (null != RoleRegistry.get().getRegisteredRole(role)) {
 
-                        result.add( new RoleImpl( role ) );
+                        result.add(new RoleImpl(role));
 
                     }
 
@@ -145,24 +146,24 @@ public class GroupAdapterAuthorizationSource {
     }
 
     /**
-     * For a given collection of principal names, return the Role instances for the ones
+     * For a given collection of principal names, return the Group instances for the ones
      * that are considered roles, so the ones that exist on the RoleRegistry.
      */
-    protected List<Group> getGroups( List<String> principals ) {
+    protected List<Group> getGroups(List<String> principals) {
 
-        if ( null != principals && !principals.isEmpty() ) {
+        if (null != principals && !principals.isEmpty()) {
 
             Set<Role> registeredRoles = RoleRegistry.get().getRegisteredRoles();
 
-            if ( null != registeredRoles && !registeredRoles.isEmpty() ) {
+            if (null != registeredRoles && !registeredRoles.isEmpty()) {
 
                 List<Group> result = new LinkedList<Group>();
 
-                for ( String role : principals ) {
+                for (String role : principals) {
 
-                    if ( null == RoleRegistry.get().getRegisteredRole( role ) ) {
+                    if (null == RoleRegistry.get().getRegisteredRole(role)) {
 
-                        result.add( new GroupImpl( role ) );
+                        result.add(new GroupImpl(role));
 
                     }
 
@@ -175,7 +176,6 @@ public class GroupAdapterAuthorizationSource {
         }
 
         return null;
-    }
-
+    }  
 
 }
