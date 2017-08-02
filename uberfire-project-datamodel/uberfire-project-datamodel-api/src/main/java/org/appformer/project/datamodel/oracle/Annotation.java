bc906ab33ea3badfcaf155acf1400a10102ff081
porcelli <alexandre.porcelli@gmail.com>
2017-08-02 07:18:51 -0400

    AF-540: updated copyrights, removed duplicated class and adjusted Errai properties file (removing additional whitespace) that breaks Errai

--- uberfire-project-datamodel/uberfire-project-datamodel-api/src/main/java/org/appformer/project/datamodel/oracle/Annotation.java
+++ uberfire-project-datamodel/uberfire-project-datamodel-api/src/main/java/org/appformer/project/datamodel/oracle/Annotation.java
@@ -1,17 +1,19 @@
 /*
- * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ *
+ */
 
 package org.appformer.project.datamodel.oracle;
 
@@ -19,7 +21,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.appformer.project.datamodel.util.PortablePreconditions;
+import org.uberfire.commons.validation.PortablePreconditions;
 
 /**
  * Portable representation of an annotation
@@ -34,8 +36,8 @@ public class Annotation {
     }
 
     public Annotation( final String qualifiedTypeName ) {
-        PortablePreconditions.checkNotNull( "qualifiedTypeName",
-                                            qualifiedTypeName );
+        PortablePreconditions.checkNotNull("qualifiedTypeName",
+                                           qualifiedTypeName );
         this.qualifiedTypeName = qualifiedTypeName;
     }
 
