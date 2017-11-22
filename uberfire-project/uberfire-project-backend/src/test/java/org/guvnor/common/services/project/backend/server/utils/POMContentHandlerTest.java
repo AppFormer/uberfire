8a7496d8bec286688a501edfa9e21e6d958b9c49
Massimiliano <desmax74@yahoo.it>
2017-11-22 10:23:34 +0100

    fix conflict

--- uberfire-project/uberfire-project-backend/src/test/java/org/guvnor/common/services/project/backend/server/utils/POMContentHandlerTest.java
+++ uberfire-project/uberfire-project-backend/src/test/java/org/guvnor/common/services/project/backend/server/utils/POMContentHandlerTest.java
@@ -26,6 +26,7 @@ import org.guvnor.common.services.project.model.POM;
 import org.junit.Test;
 
 import static java.util.stream.Collectors.toList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
